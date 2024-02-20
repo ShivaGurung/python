d56aa2883c8f5335e96f3abf16d007070a1b4f8b
ShivaGurung <158254979+ShivaGurung@users.noreply.github.com>
2024-02-20 12:06:13 +0545

    Add files via upload

---
+++ datastrucutre.py
@@ -0,0 +1,14 @@
+# my_list
+
+my_list = ['apple','ball','cat','dog']
+print (my_list[0])
+print (my_list[1])
+print (my_list[2])
+print (my_list[3])
+print (my_list[4])
+
+my_list = ['a','b','c','d','e',1,2,True,False]
+
+print (len(my_list))
+for i in range (0, len(my_list)):
+    print(my_list[i])
---
+++ program1.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20.55
+name = "Shiva"
+print (type(a))
+print (type(name))
+
+sum = a+b
+print(sum)
+print(type(sum))
+
+c = 10
+d = 0
+
+output = c/d
+print(output)
